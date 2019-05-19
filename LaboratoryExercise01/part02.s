2873e2bcc438510759df3c1f78ab31b5de734d33
abdulaziz5343 <41600587+abdulaziz5343@users.noreply.github.com>
2019-05-19 16:28:52 +0900

    Find largest number in a list

--- LaboratoryExercise01/part02.s
+++ LaboratoryExercise01/part02.s
@@ -1,55 +1,20 @@
-	.include "address_map_nios2.s"
-/******************************************************************************
- * This program demonstrates use of parallel ports
- *
- * It performs the following: 
- * 	1. displays the SW switch values on the red LEDs
- * 	2. displays the KEY pushbutton values on the green LEDs
- * 	3. displays a rotating pattern on the HEX displays
- * 	4. if any KEY is pressed, the SW switches are used as the rotating pattern
- ******************************************************************************/
-	.text						# executable code follows
-	.global	_start
+.text
+.global  _start
 _start:
-
-	/* initialize base addresses of parallel ports */
-	movia	r15, SW_BASE		# SW slider switch base address
-	movia	r16, LEDR_BASE		# red LED base address
-	movia	r17, KEY_BASE		# pushbutton KEY base address
-	movia	r18, LEDG_BASE		# green LED base address
-	movia	r20, HEX3_HEX0_BASE	# HEX3_HEX0 base address
-	movia	r21, HEX7_HEX4_BASE	# HEX7_HEX4 base address
-	movia	r19, HEX_bits
-	ldwio	r6, 0(r19)			# load pattern for HEX displays
-
-DO_DISPLAY:
-	ldwio	r4, 0(r15)			# load slider switches
-	stwio	r4, 0(r16)			# write to red LEDs
-
-	ldwio	r5, 0(r17)			# load pushbuttons
-	stwio	r5, 0(r18)			# write to green LEDs
-	beq		r5, r0, NO_BUTTON	
-	mov		r6, r4				# copy SW switch values onto HEX displays
-WAIT:
-	ldwio	r5, 0(r17)			# load pushbuttons
-	bne		r5, r0, WAIT		# wait for button release
-
-NO_BUTTON:
-	stwio	r6, 0(r20)			# store to HEX3 ... HEX0
-	stwio	r6, 0(r21) 			# store to HEX7 ... HEX4
-	roli	r6, r6, 1			# rotate the displayed pattern
-
-	movia	r7, 1000000			# delay counter
-DELAY:	
-	subi	r7, r7, 1
-	bne		r7, r0, DELAY	
-
-	br		DO_DISPLAY
-
-/******************************************************************************/
-	.data						# data follows
-
-HEX_bits:
-	.word 0x0000000F
-
-	.end
+		movia		r8, RESULT	#r8 points to result loacation
+		ldw 		r4, 4(r8)	#r4 is a counter, initialize it with N
+		addi		r5, r8, 8	#r5 points to the first number
+		ldw		r2, (r5)	#r2 holds the largest number found so far
+LOOP:		subi		r4 ,r4, 1	#decrement the counter
+		beq		r4, r0,DONE	#finished if r4 is equal to 0
+		addi 		r5,r5,4		#increment the list pointer
+		ldw		r6, (r5)	#get the next number
+		bge		r2, r6,LOOP     #check if larger number found
+		mov 		r2, r6		#update the largest found
+		br 		LOOP
+DONE:		stw		r2, (r8)	#store the largest number into RESULT
+STOP:		br		STOP		#remain here when done
+RESULT:		.skip		4		#space for the largest number found
+N:		.word		7
+NUMBERS:	.word		4,5,3,6,1,8,2 #numbers in the list
+		.end
